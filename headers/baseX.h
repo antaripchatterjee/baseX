9f4eda3cca47881d09228d26c91df226f13caac7
Antarip Chatterjee <antarip.chatterjee22@gmail.com>
2025-01-20 15:29:50 +0000

    baseX.h header modified

--- headers/baseX.h
+++ headers/baseX.h
@@ -3,8 +3,10 @@
 
 #ifdef MAKEFILECOMPILE
 #include "base64.h"
+#include "base32.h"
 #else
 #include "../headers/base64.h"
+#include "../headers/base32.h"
 #endif // MAKEFILECOMPILING
 
 #endif // __BASEX_H__
--- test/base64_test.c
+++ test/base64_test.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "headers/baseX.h"
+#include "baseX.h"
 
 int main(int argc, const char* argv[]) {
     if(argc < 2) {
